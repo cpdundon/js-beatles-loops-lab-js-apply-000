8932ae454bf554d8c7a28bbfc449cf4397895821
Christopher P. Dundon <chris.dundon@gmail.com>
2018-01-28 01:02:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
 
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
 function theBeatlesPlay() {
   console.log("TEST")
