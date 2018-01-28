3d1209d6961d8bf0f3115b9769060915fcd15419
Christopher P. Dundon <chris.dundon@gmail.com>
2018-01-28 01:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(m, inst)  {
-  var l = m.length - 1
+  var l = m.length
+  
+  var step
+  for (step = 0; step < l; step++) {
+  console.log('Walking east one step');
+}
   
   console.log(l)  
 }
