f14ee871c6880131dde8487209b5037e27f06001
Christopher P. Dundon <chris.dundon@gmail.com>
2018-01-28 01:36:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function theBeatlesPlay(m, inst)  {
   var l = m.length
   
@@ -14,7 +21,7 @@ function theBeatlesPlay(m, inst)  {
 }
 
 
-function johnLennonFacts () {
+function johnLennonFacts (f) {
   
   
 }
